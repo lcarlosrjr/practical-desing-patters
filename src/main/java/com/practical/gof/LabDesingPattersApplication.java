fa339b77b2873f2eb5185e5460443ac45695101e
ljunior <ljunior@padtec.com.br>
2024-02-28 22:54:58 -0300

    base project

---
+++ src/main/java/com/practical/gof/LabDesingPattersApplication.java
@@ -0,0 +1,13 @@
+package com.practical.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDesingPattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDesingPattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practical/gof/LabDesingPattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDesingPattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
